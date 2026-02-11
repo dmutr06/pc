19d38444505b784de9a1d4fd8c120a0c9d662b2b
Dmytro Shadura <shaduraa.dmutroo@gmail.com>
2026-02-11 22:41:47 +0200

    feat (lab1): implement matrix filling

--- lab1/main.cpp
+++ lab1/main.cpp
@@ -1,5 +1,31 @@
 #include <print>
+#include <cstdio>
+#include <cstdlib>
+#include <ctime>
+#include <vector>
+#include <random>
+
+void fill_matrix(std::vector<int> &matrix) {
+    std::random_device rd;
+    std::mt19937 eng(rd());
+    std::uniform_int_distribution dist(1, 100);
+
+    for (int &cell : matrix) {
+        cell = dist(eng);
+    }    
+}
 
 int main(int argc, char **argv) {
-    std::println("Hello, World!"); 
+    if (argc < 2) {
+        std::println(stderr, "Please specify matrix size");
+        return 1;
+    }
+
+    const int matrix_size = atoi(argv[1]);
+
+    std::vector<int> matrix(matrix_size * matrix_size);
+
+    fill_matrix(matrix);
+
+    std::println("{}", matrix);
 }
